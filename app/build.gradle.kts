9506fa50be37b2c7c65b73ea3af6683cb80fb93b
pachospm <pachosp123@gmail.com>
2026-04-27 08:26:45 -0500

    config: dependencias del modulo app y configuracion de compilacion

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -39,7 +40,28 @@ android {
     }
 }
 
+kotlin{
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
+    }
+
+}
+
 dependencies {
+
+    implementation("androidx.navigation:navigation-compose:2.9.8")
+
+    //ROOM
+    implementation("androidx.room:room-runtime:2.8.4")
+    implementation("androidx.room:room-ktx:2.8.4")
+    add("ksp", "androidx.room:room-compiler:2.8.4")
+
+    //Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
+
+    // OpenStreetMap
+    implementation("org.osmdroid:osmdroid-android:6.1.20")
+
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.compose.material3)
